55c8e8612575b37d4aa5f535d3e2af7186e6d7c1
dandan.zheng <cha.baby@qq.com>
2018-04-04 21:35:58 +0800

    modify

--- SpiderGithub.py
+++ SpiderGithub.py
@@ -17,13 +17,12 @@ class SpiderGithub(scrapy.Spider):
     def start_urls(self):
         url_template = 'https://github.com/shiyanlou?page={}&tab=repositories'
         urls = (url_template.format(i) for i in range(1, 5))
-        print(urls)
         return urls
 
     def parse(self, response):
-        for source in response.xpath('//*[@id="user-repositories-list"]'):
+        for source in response.css('div#user-repositories-list li'):
             yield{
                 # itemprop="name codeRepository"
-                'name': source.xpath('.//div[contains(@class, "mb-1")]/h3/a/text()').re_first('\S+'),
-                'update_time': source.xpath('.//relative-time/@datetime').extract_first()
+                'name': source.css('a::text').re_first('\S+'),
+                'update_time': source.css('relative-time::attr("datetime")').extract_first()
             }
